384a9052889644399e0ab545a954b4b510c5b623
Kacper Fornalczyk <fk46509@zut.edu.pl>
2025-03-15 19:20:49 +0100

    Modifing start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-rm -rf /opt/venv
-python -m venv /opt/venv
-source /opt/venv/bin/activate
-pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port $PORT
+pip install -r requirements.txt && uvicorn main:app --host 0.0.0.0 --port $PORT
